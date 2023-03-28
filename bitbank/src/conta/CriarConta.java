f43082cc7693a25370dc3f9a3b664d93ccdd4821
Douglas Vilela <douglasvilelaa@gmail.com>
2023-03-27 23:14:25 -0300

    projeto banco

---
+++ bitbank/src/conta/CriarConta.java
@@ -0,0 +1,20 @@
+package conta;
+
+public class CriarConta {
+	
+	
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		
+		primeiraConta.setAgencia(1);
+		primeiraConta.setNumero(10);
+		primeiraConta.setSaldo(200.00);
+		System.out.println(primeiraConta.getSaldo());
+
+
+		primeiraConta.depositaDinheiro(100.00);
+		
+		System.out.println(primeiraConta.getSaldo());
+	}
+
+}
---
+++ bitbank/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Vilela
+ *
+ */
+module bitbank {
+}
